e892a9ca6897f342e60ae6c0e74ec0f79cb26c41
kog1120 <kog1120@gmail.com>
2017-03-25 17:00:37 +0900

    a
    a

--- main.js
+++ main.js
@@ -2,10 +2,18 @@ const Telegraf = require('telegraf')
  
 const app = new Telegraf("365502316:AAHrAhpajKXA_Wf2ChVSuflpCKTojPqqd3w")
  
+
+// 기본적인 커맨드
 app.command('tanosi', (ctx) => {
-  console.log('start', ctx.from)
+  console.log('tanosi', ctx.from)
   ctx.reply('와이~ 타노시이!')
 })
+
+app.command('sukgoi', (ctx) => {
+  console.log('sukgoi', ctx.from)
+  ctx.reply('슥고이!!')
+})
+
  
 app.hears('hi', (ctx) => ctx.reply('Hey there!'))
  
