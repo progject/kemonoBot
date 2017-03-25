7a030c50d73e5d638dbe0f95d67afe8f14c5c087
kog1120 <kog1120@gmail.com>
2017-03-25 17:58:58 +0900

    1.0.1.2
    2

--- main.js
+++ main.js
@@ -9,11 +9,6 @@ app.command('tanosi', (ctx) => {
   ctx.reply('와이~ 타노시이!')
 })
 
-app.command('tanosi@kemonoBot', (ctx) => {
-  console.log('tanosi', ctx.from)
-  ctx.reply('와이~ 타노시이!s')
-})
-
 app.command('sukgoi', (ctx) => {
   console.log('sukgoi', ctx.from)
   ctx.reply('슥고이!!')
@@ -22,6 +17,17 @@ app.command('sukgoi', (ctx) => {
 app.command('test', (ctx) => {
   console.log('test', ctx.from)
   ctx.reply(ctx.from.first_name + '은(는) 바보같은 프렌즈구나')
+})
+
+// 여러 사용자가 있을때
+app.command('tanosi@kemonoBot', (ctx) => {
+  console.log('tanosis', ctx.from)
+  ctx.reply('와이~ 타노시이!')
+})
+
+app.command('sukgoi@kemonoBot', (ctx) => {
+  console.log('sukgois', ctx.from)
+  ctx.reply('슥고이!!')
 })
 
  
