dc500a0f0ae80410fd72ccacc3d2a9c408932091
kog1120 <kog1120@gmail.com>
2017-03-25 16:45:21 +0900

    t
    t

--- main.js
+++ main.js
@@ -2,9 +2,9 @@ const Telegraf = require('telegraf')
  
 const app = new Telegraf("365502316:AAHrAhpajKXA_Wf2ChVSuflpCKTojPqqd3w")
  
-app.command('start', (ctx) => {
+app.command('tanosi', (ctx) => {
   console.log('start', ctx.from)
-  ctx.reply('Welcome!')
+  ctx.reply('와이~ 타노시이!')
 })
  
 app.hears('hi', (ctx) => ctx.reply('Hey there!'))
