125af1af5d7b416b5e780c813231e0c0253bffa1
gproj <kog1120@gmail.com>
2017-10-04 21:53:54 +0900

    Update app.js

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use('/', bot);
 
-app.listen(process.env.PORT, function () {
-  console.log('Example app listening on port '+process.env.PORT);
+app.listen(8080, function () {
+  console.log('kemokemo ');
 });
 
 module.exports = app;
